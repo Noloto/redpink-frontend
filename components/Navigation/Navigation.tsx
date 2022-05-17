7387b3a8e3890d318e72ce3fb10921c8979d030f
Noël Willener <noel.willener@swisscom.com>
2022-05-17 08:16:57 +0200

    style: make circle above cart perfectly rounded

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -230,7 +230,7 @@ const Navigation: React.FC<RequiredProps & OptionalProps> = ({
                   />
                 </svg>
                 {cart?.products && cart?.products?.length > 0 && (
-                  <p className="flex text-xs border-redpink rounded-full border-4bi bg-redpink text-white h-4">
+                  <p className="flex text-xs border-redpink rounded-full border-4 bg-redpink text-white h-4 items-center">
                     <div>{cart.products.length}</div>
                   </p>
                 )}
