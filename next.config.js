f779fe94fe44de03e2fec2f6d82b0703337284b4
Noël Willener <noel.willener@swisscom.com>
2022-08-08 15:47:23 +0200

    feat: maintenance mode

--- next.config.js
+++ next.config.js
@@ -9,6 +9,8 @@ const nextConfig = {
   env: {
     STOREFRONT_API_URL: process.env.STOREFRONT_API_URL,
     STOREFRONT_ACCESS_TOKEN: process.env.STOREFRONT_ACCESS_TOKEN,
+    MAINTENANCE: process.env.MAINTENANCE,
+    PASSWORD: process.env.PASSWORD,
   },
 };
 
