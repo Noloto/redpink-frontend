26f3e21e7fb5328a802fc4291bd51e691ff29d60
Noël Willener <noel.willener@swisscom.com>
2023-08-04 14:33:58 +0200

    feat(mail): add content to mail component

--- components/Emails/Want.tsx
+++ components/Emails/Want.tsx
@@ -1,26 +1,53 @@
 import { Button } from '@react-email/button';
+import { Heading } from '@react-email/heading';
 import { Html } from '@react-email/html';
-import * as React from 'react';
+import { Img } from '@react-email/img';
+import { Text } from '@react-email/text';
+
+import banner from '/images/banner.webp';
+import Image from 'next/image';
 
 type RequiredProps = {
   productName: string;
   productImage: string;
-  senderEmail: string;
 };
 
-const Want: React.FC = ({}) => {
+const Want: React.FC<RequiredProps> = ({ productName, productImage }) => {
   return (
     <Html>
+      <Heading as="h1">Added to List</Heading>
       <Button
         pX={20}
         pY={12}
         href="https://redpink.pink"
         style={{ background: '#000', color: '#fff' }}
       >
-        hey you. thank you for the interest in a piece that’s not out yet. Love
+        <Image src={banner} alt="Redpink banner" width="300" height="300" />
+      </Button>
+      <Text style={{ color: 'white' }}>
+        Hey you. thank you for the interest in a piece that’s not out yet. Love
         gets love ! you will get priority on this piece , early access/privat
         code to bye… stay updated ! Peace
-      </Button>
+      </Text>
+      <Img src={productImage} alt="Product Image" width="300" height="300" />
+      <Text style={{ color: 'white' }}>YOUR ADDED TO LIST</Text>
+      <Text style={{ color: 'white' }}>{productName}</Text>
+      <Text style={{ color: 'white' }}>=</Text>
+      <Text style={{ color: 'white' }}>EARLY ACCESSs</Text>
+      <Text style={{ color: 'white' }}>+</Text>
+      <Image
+        src="/images/banner.webp"
+        alt="Whatever"
+        width="300"
+        height="300"
+      />
+      <Text style={{ color: 'white' }}>LOVE GETS LOVE</Text>
+      <Image
+        src="/images/background.webp"
+        alt="Background Image"
+        width="300"
+        height="300"
+      />
     </Html>
   );
 };
