353c2aed2e4b52f73269f723b2a7510bef08b547
Noël Willener <noel.willener@swisscom.com>
2023-08-07 11:45:50 +0200

    chore: placeholder for req

--- components/Emails/Want.tsx
+++ components/Emails/Want.tsx
@@ -3,9 +3,13 @@ import { Heading } from '@react-email/heading';
 import { Html } from '@react-email/html';
 import { Img } from '@react-email/img';
 import { Text } from '@react-email/text';
+import { Section } from '@react-email/section';
+import { Font } from '@react-email/font';
 
-import banner from '/images/banner.webp';
+import banner from '../../public/images/banner.webp';
+import background from '../../public/images/backgrounds/bg3.jpeg';
 import Image from 'next/image';
+import { Eina } from '../../common/utils/fonts/fonts';
 
 type RequiredProps = {
   productName: string;
@@ -15,39 +19,45 @@ type RequiredProps = {
 const Want: React.FC<RequiredProps> = ({ productName, productImage }) => {
   return (
     <Html>
-      <Heading as="h1">Added to List</Heading>
-      <Button
-        pX={20}
-        pY={12}
-        href="https://redpink.pink"
-        style={{ background: '#000', color: '#fff' }}
-      >
-        <Image src={banner} alt="Redpink banner" width="300" height="300" />
-      </Button>
-      <Text style={{ color: 'white' }}>
-        Hey you. thank you for the interest in a piece that’s not out yet. Love
-        gets love ! you will get priority on this piece , early access/privat
-        code to bye… stay updated ! Peace
-      </Text>
-      <Img src={productImage} alt="Product Image" width="300" height="300" />
-      <Text style={{ color: 'white' }}>YOUR ADDED TO LIST</Text>
-      <Text style={{ color: 'white' }}>{productName}</Text>
-      <Text style={{ color: 'white' }}>=</Text>
-      <Text style={{ color: 'white' }}>EARLY ACCESSs</Text>
-      <Text style={{ color: 'white' }}>+</Text>
-      <Image
-        src="/images/banner.webp"
-        alt="Whatever"
-        width="300"
-        height="300"
+      <Font
+        fontFamily="Noto Serif Vithkuqi"
+        fallbackFontFamily="Arial"
+        webFont={{
+          url: 'https://fonts.googleapis.com/css2?family=Noto+Serif+Vithkuqi&display=swap',
+          format: 'woff2',
+        }}
+        fontWeight={400}
+        fontStyle="normal"
       />
-      <Text style={{ color: 'white' }}>LOVE GETS LOVE</Text>
       <Image
-        src="/images/background.webp"
+        src={background}
         alt="Background Image"
-        width="300"
-        height="300"
+        fill={true}
+        style={{
+          objectFit: 'cover',
+          objectPosition: 'center',
+          zIndex: 0,
+        }}
       />
+      <Section style={{ position: 'relative', zIndex: 1 }}>
+        <Heading as="h1">Added to List</Heading>
+        <Button pX={0} pY={0} href="https://redpink.pink">
+          <Image src={banner} alt="Redpink banner" width="300" height="200" />
+        </Button>
+        <Text>
+          Hey you. thank you for the interest in a piece that’s not out yet.
+          Love gets love ! you will get priority on this piece , early
+          access/privat code to bye… stay updated ! Peace
+        </Text>
+        <Img src={productImage} alt="Product Image" width="300" height="300" />
+        <Text>YOUR ADDED TO LIST</Text>
+        <Text>{productName}</Text>
+        <Text>=</Text>
+        <Text>EARLY ACCESSs</Text>
+        <Text>+</Text>
+        <Image src={banner} alt="Whatever" width="300" height="300" />
+        <Text style={{ color: 'white' }}>LOVE GETS LOVE</Text>
+      </Section>
     </Html>
   );
 };
